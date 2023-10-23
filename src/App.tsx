6d0ecb622feba0022f19fbba0ddffd5cc81adb67
Muradil Koychubekov <muradil.koychubekob@gmail.com>
2023-10-23 23:56:09 +0600

    created components Form and Task

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import AddTaskForm from './component/AddTaskForm/AddTaskForm.tsx';
+import Task from './component/Task/Task.tsx';
 
 const App = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="container mx-auto my-5">
+      <AddTaskForm/>
+      <Task/>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/component/Task/Task.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Task = () => {
+  return (
+    <div className='border mt-4'>
+      <h1>Clean home</h1>
+    </div>
+  );
+};
+
+export default Task;
