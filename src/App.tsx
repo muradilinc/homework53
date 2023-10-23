9db79263c871c8d6bee6b0b0eb6ee552f5d3148b
Muradil Koychubekov <muradil.koychubekob@gmail.com>
2023-10-24 00:07:00 +0600

    adding task

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import {useState} from 'react';
 import AddTaskForm from './component/AddTaskForm/AddTaskForm.tsx';
 import Task from './component/Task/Task.tsx';
 
+interface Task {
+  id: Date;
+  taskText: string;
+  statusTask: boolean;
+}
+
 const App = () => {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  const addTask = () => {
+    const task: Task = {
+      id: new Date(),
+      taskText: 'Cook cakes',
+      statusTask: false
+    };
+
+    const taskArray = [...tasks];
+    taskArray.push(task);
+
+    setTasks(taskArray);
+  };
+
+  console.log(tasks);
+
   return (
     <div className="container mx-auto my-5">
-      <AddTaskForm/>
+      <AddTaskForm addButton={addTask}/>
       <Task/>
     </div>
   );
--- src/component/AddTaskForm/AddTaskForm.tsx
+++ src/component/AddTaskForm/AddTaskForm.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
 
-const AddTaskForm: React.FC = () => {
+interface Props {
+  addButton: React.MouseEventHandler;
+}
+
+const AddTaskForm: React.FC<Props> = ({addButton}) => {
   return (
     <React.Fragment>
-
       <form>
         <label htmlFor="search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
         <div className="relative">
           <input type="search" id="search"
                  className="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                  placeholder="Hacking Nasa..." required/>
-          <button type="submit"
+          <button type="button"
+                  onClick={addButton}
                   className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add
           </button>
         </div>
       </form>
-
     </React.Fragment>
   );
 };
